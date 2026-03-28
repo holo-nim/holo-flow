e1c7d9926ab7906a478788c216c94923f1f7f67f
metagn <metagngn@gmail.com>
2026-03-28 15:22:31 +0300

    don't build docs for api files

--- tasks/build_docs.nim
+++ tasks/build_docs.nim
@@ -7,4 +7,12 @@ when not declared(buildDocs):
   {.error: "docs task not implemented, need nimbleutils".}
 
 # run from project root
-buildDocs(gitUrl = "https://github.com/holo-nim/holo-flow")
+
+import std/[os, strutils]
+
+let srcDir = "src"
+var files: seq[FilePath] = @[]
+for f in walkDirRec(srcDir):
+  if f.endsWith(".nim") and not f.endsWith("_api.nim"):
+    files.add f
+buildDocs(files, gitUrl = "https://github.com/holo-nim/holo-flow")
